d5da89d7b6411ec7815a52d0c05fe10533cf2e5d
Earl-CS <ejconag@gmail.com>
2020-12-19 21:46:30 +0800

    added navbar and sidenav but yet to be polished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <!-- <SideNav/> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from '@/components/partials/Navbar'
+// import SideNav from '@/components/partials/SideNav'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    // SideNav
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <nav class="white z-depth-0">
+            <div class="nav-wrapper container ">
+            
+            <span>
+                <!-- <a href="#" data-target="slide-out" class="sidenav-trigger black-text">click</a> -->
+                <a href="#" data-target="slide-out" class="sidenav-trigger black-text btn show-on-large"><i class="material-icons">menu</i></a>
+                <router-link to="/" class="brand-logo cyan-text text-accent-4">BusTap</router-link>
+            </span>
+        
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html" class="black-text">Sass</a></li>
+                <li><a href="badges.html"  class="black-text">Components</a></li>
+                <li><a href="collapsible.html"  class="black-text">JavaScript</a></li>
+            </ul>
+            </div>
+        </nav>
+
+        <ul id="slide-out" class="sidenav">
+            <li>
+                <div class="user-view">
+                    <div class="background">
+                        <img src="images/office.jpg">
+                    </div>
+                    <a href="#name"><span class="white-text name">John Doe</span></a>
+                    <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+                </div>
+            </li>
+            <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+            <li><a href="#!">Second Link</a></li>
+            <li><div class="divider"></div></li>
+            <li><a class="subheader">Subheader</a></li>
+            <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+        </ul>
+
+    <!-- <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a> -->
+    </div>
+    
+</template>
