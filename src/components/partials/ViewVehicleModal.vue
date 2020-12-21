d68d4584f020e1bd1592328a021fe643a0f2d710
JustinRaz <58808342+JustinRaz@users.noreply.github.com>
2020-12-21 11:43:46 +0800

    --- Auto-Git Commit ---

--- src/components/partials/ViewVehicleModal.vue
+++ src/components/partials/ViewVehicleModal.vue
@@ -9,17 +9,17 @@
                     <div class="row">
                         <div class="input-field col s6">
                             <label for="bus_code">Bus Code</label>
-                            <input id="bus_code" type="text" class="validate" v-model="bus_code" :value="vehicle.bus_code">
+                            <input :id="'bus_code-'+ vehicle.uid" type="text" class="validate" :value="vehicle.bus_code">
                         </div>
                         <div class="input-field col s6">
                             <label for="plate_number">Plate Number</label>
-                            <input id="plate_number" type="text" class="validate" :value="vehicle.plate_number">
+                            <input :id="'plate_number-'+ vehicle.uid" type="text" class="validate" :value="vehicle.plate_number">
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
                             <label for="type">Vehicle Type</label>
-                            <input id="type" type="text" class="validate" :value="vehicle.type">
+                            <input :id="'type-'+ vehicle.uid" type="text" class="validate" :value="vehicle.type">
                         </div>
                     </div>
                     <div class="row">
@@ -39,25 +39,25 @@
 </template>
 
 <script>
-// import db from '../firebase/firebaseInit'
+import db from '../firebase/firebaseInit'
 
 import M from 'materialize-css'
 
 export default {
     props: ['vehicle'],
-    data: {
-        bus_code: '',
-        plate_number: '',
-        type: '',
-    },
-    data(){
-    //    return {
-    //        vehicles: []
-    //    } 
-    },
     methods: {
         update: function(){
-            console.log(this.bus_code);
+            var busCode = document.getElementById('bus_code-'+this.vehicle.uid).value;
+            var plateNumber = document.getElementById('plate_number-'+this.vehicle.uid).value;
+            var type = document.getElementById('type-'+this.vehicle.uid).value;
+            db.collection('buses').doc(this.vehicle.uid).update({
+                'bus_code': busCode,
+                'plate_number': plateNumber,
+                'type': type
+            }).then(()=>{
+                    console.log(busCode, plateNumber, type);
+                }
+            );
         }
     },
     created(){
